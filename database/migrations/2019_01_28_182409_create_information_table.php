47c12305ebb8dde44921c36a85e2d5653e229ad6
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2019-01-29 07:33:41 +0100

    commecer edition des informations

---
+++ database/migrations/2019_01_28_182409_create_information_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('information', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('raison');
+            $table->string('matricule');
+            $table->string('registre');
+            $table->string('adresse');
+            $table->string('tel');
+            $table->string('email');
+            $table->binary('logo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('information');
+    }
+}
